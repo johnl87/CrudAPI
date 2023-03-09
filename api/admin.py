e2dcbbe172dd350129d82f11dbe363a64d020704
John Le <65139320+johnl87@users.noreply.github.com>
2023-03-08 21:29:51 -0500

    created user account

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Location, Item
+admin.site.register(Item)
+admin.site.register(Location)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    locationName = models.CharField(max_length=100, unique=True)
+
+    #provides name instead of ID
+    def __str__(self):
+        return self.locationName
+    
+class Item(models.Model):
+    itemName = models.CharField(max_length=100)
+    date_installed = models.DateField(auto_now_add=True)
+    #when model is deleted, all the items at that location is deleted
+    itemLocation = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    #string representation of item
+    def __str__(self):
+        return self.itemName
